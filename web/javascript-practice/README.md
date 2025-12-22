39ddc47aa04c2753b2840d36931c784e65d3de3a
Ben Wright <ben31w@gmail.com>
2025-12-22 13:10:35 -0500

    replace inline HTML event handling with event listeners

--- web/javascript-practice/README.md
+++ web/javascript-practice/README.md
@@ -3,6 +3,12 @@ Introduction to JavaScript.
 
 All Vanilla JS. No frameworks.
 
-Takes inspiration from "A Smarter Way to Learn JavaScript".
+I started this effort around 2023, taking inspiration from "A Smarter 
+Way to Learn JavaScript". 
 
-Best starting point might be hello.html/hello.js
+Best starting point might be hello.html + hello.js. These examples are very 
+simple, and I've tried to annotate them and keep them in good modern practice.
+
+Old starting point that could still be helpful is index.html + index.js. These
+are less organized and include some poor practices like inline event handling (ex:
+onclick, onsubmit, etc.)
--- web/javascript-practice/hello.js
+++ web/javascript-practice/hello.js
@@ -1,12 +1,19 @@
+// ===============
+// FUNCTIONS
+// ===============
 /**
  * Greet user by displaying msg at console and web page.
  * @param {} name 
  */
 function greet(name) {
-    let fstr = `Hello, ${name}`;  // Format String
-    console.log(fstr);  // Console logging
-    document.getElementById("greeting-p").innerHTML = fstr;  // Adjusting inner HTML
+    if (name != null) {
+        let fstr = `Hello, ${name}`;  // Format String
+        console.log(fstr);  // Console logging
+        // Adjusting inner HTML
+        document.getElementById("greeting-p").innerHTML = fstr;
+    }
 }
+
 
 /**
  * Validate form upon submission.
@@ -15,12 +22,21 @@ function greet(name) {
 function validateForm() {
     let name = document.getElementById("name").value;
     greet(name);
-
-    // Note on returning (for an onsubmit function):
-    // Usually, when a form is submitted, the page is refreshed (or a new page is 
-    // loaded). If you adjust innerHTML (as this script does), your change is 
-    // lost upon refresh. You can return false to prevent this.
-    // Typically, a form submits a POST request to a server and navigates to a 
-    // new page. So this isn't a problem 99.99% of the time.
-    return false;
 }
+
+
+// ===============
+// EVENT LISTENERS
+// ===============
+document.getElementById("greeting-form").addEventListener(
+    'submit', 
+    function(event) {
+        // Usually, when a form is submitted, the page is refreshed (or a new page 
+        // is loaded). If you adjust innerHTML (as this script does), your change 
+        // is lost upon refresh. Call "preventDefault" to prevent this.
+        // Typically, a form submits a POST request to a server and navigates to a 
+        // new page. So this isn't a problem 99.99% of the time.
+        event.preventDefault();
+        validateForm();
+    }
+);
