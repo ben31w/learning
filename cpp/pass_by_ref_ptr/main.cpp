f1ce1d40baa6efd0e85963cfbac4fde04be95edc
Ben Wright <ben31w@gmail.com>
2026-02-17 09:46:07 -0500

    demo some fancier C++ techniques like passing a reference to a pointer

--- cpp/pass_by_ref_ptr/main.cpp
+++ cpp/pass_by_ref_ptr/main.cpp
@@ -5,60 +5,107 @@
 
 using namespace std;
 
-//  Syntax              Term                            Desc
-//  T arg               Pass by value                   shallow copy is passed; OG var isn't modified
-//  T& arg              Pass by reference               OG var is passed and modified. Reference to OG var = OG var.
-//  const T& arg        Pass by const reference         OG var is passed but not modified.
-//  T* arg              Pass by pointer                 pointer to OG var is passed. Pointer and variable pointed to are modified.
-//  const T* arg        Pass by pointer to const        pointer to OG var is passed. Pointer can be modified; variable pointed to cannot.
-//  T* const arg        Pass by const pointer           pointer to OG var is passed. Pointer cannot be modified; variable pointed to can.
-//  const T* const arg  Pass by const pointer to const  pointer to OG var is passed. Pointer and variable pointed to cannot be modified.
+// Syntax              Term                            Desc
+// T arg               Pass by value                   shallow copy is passed; OG var isn't modified
+// T& arg              Pass by reference               OG var is passed and modified. Reference to OG var = OG var.
+// const T& arg        Pass by const reference         OG var is passed but not modified.
+// T* arg              Pass by pointer                 copy of pointer to OG var is passed. Pointer copy and var pointed to are modified.
+// const T* arg        Pass by pointer to const        copy of pointer to OG var is passed. Pointer copy can be modified; var pointed to cannot.
+// T* const arg        Pass by const pointer           copy of pointer to OG var is passed. Pointer copy cannot be modified; var pointed to can.
+// const T* const arg  Pass by const pointer to const  copy of pointer to OG var is passed. Pointer copy and var pointed to cannot be modified.
 
-// Pass by value is rarely used because copying can be expensive.
-// If you want to avoid modifications to OG var, use const reference, pointer to const, or const pointer to const.
+// T** arg              Pass by pointer to pointer                      pointer to pointer to OG var is passed. Pointer and var pointed to are modified.
+// T*& arg              Pass by reference to pointer                    pointer to OG var is passed. Pointer and var pointed to are modified. Preferred.
+// const T*& arg        Pass by reference to pointer to const           pointer to OG var is passed. Pointer can be modified; var pointed to cannot.
+// const T* const& arg  Pass by reference to const pointer to const     pointer to OG var is passed. Pointer and var pointed to cannot be modified.
 
-void passByVal(int i) {
-    i += 1;  // modifies a copy.
+// NOTES
+// - Pass by value is rarely used because copying can be expensive.
+//   If you want to avoid modifications to OG var, use const reference, pointer to const, or const pointer to const.
+// - Note, pass by pointer really passes a copy to a pointer. Similar to pass by value. The pointer can be re-assigned,
+//   but only temporarily, because it's a copy. To access the pointer itself, pass by reference to pointer.
+
+void passByVal(int i)
+{
+    i += 1; // modifies a copy.
 }
 
-void passByRef(int& i) {
-    i += 1;  // modifies OG var
+void passByRef(int &i)
+{
+    i += 1; // modifies OG var
 }
 
-void passByConstRef(const int& i) {
+void passByConstRef(const int &i)
+{
     // i += 1;  // not allowed
 }
 
-void passByPtr(int* p) {
-    if (p == nullptr) {
-        return;
-    }
-    *p += 1;  // de-references ptr to modify OG var
+void passByPtr(int *p)
+{
+    if (p == nullptr) return;
+    
+    *p += 1; // de-references ptr to modify OG var
 }
 
-void passByPtrToConst(const int* p) {
-    if (p == nullptr) {
-        return;
-    }
+void passByPtrToConst(const int *p)
+{
+    if (p == nullptr) return;
+    
     // *p += 1;  // not allowed
 }
 
-void passByConstPtr(int* const p) {
-    if (p == nullptr) {
-        return;
-    }
-    *p += 1;  // de-references ptr to modify OG var
+void passByConstPtr(int *const p)
+{
+    if (p == nullptr) return;
+    
+    *p += 1; // de-references ptr to modify OG var
 }
 
-void passByConstPtrToConst(const int* const p) {
-    if (p == nullptr) {
-        return;
-    }
+void passByConstPtrToConst(const int *const p)
+{
+    if (p == nullptr) return;
+    
     // *p += 1;  // not allowed
 }
 
+void passByPtrToPtr(int **p)
+{
+    if (p == nullptr) return;
 
-int main() {
+    static int x = 20;
+    *p = &x;
+    **p += 1;
+}
+
+void passByRefToPtr(int*& p)
+{
+    if (p == nullptr) return;
+
+    static int x = 30;
+    p = &x;
+    *p += 1;
+}
+
+void passByRefToPtrToConst(const int*& p) {
+    if (p == nullptr) return;
+
+    static int x = 40;
+    p = &x;
+    // *p += 1;  // not allowed
+}
+
+void passByRefToConstPtrToConst(const int* const& p) {
+    if (p == nullptr) return;
+
+    static int x = 40;
+    // p = &x;  // not allowed
+    // *p += 1;  // not allowed
+}
+
+void demoPass()
+{
+    cout << "\n====Pass variables and references: pointers don't change.====" << endl;
+
     int i = 10;
 
     cout << "----initialization----" << endl;
@@ -98,6 +145,49 @@ int main() {
 
     cout << "----passByConstPtrToConst----" << endl;
     cout << "i=" << i << endl;
+}
+
+void demoPassFancy()
+{
+    cout << "\n====Pass pointer variables: pointers change.====" << endl;
+
+    int i = 10;
+    int *p = &i;
+
+    cout << "----initialize----" << endl;
+    cout << "   i = " << i << endl;
+    cout << "  *p = " << *p << endl;
+
+    passByPtrToPtr(&p);
+
+    cout << "----passByPtrToPtr----" << endl;
+    cout << "   i = " << i << endl;
+    cout << "  *p = " << *p << endl;
+
+    passByRefToPtr(p);
+
+    cout << "----passByRefToPtr----" << endl;
+    cout << "   i = " << i << endl;
+    cout << "  *p = " << *p << endl;
+
+    // passByRefToPtrToConst(p);
+
+    // cout << "----passByRefToPtrToConst----" << endl;
+    // cout << "   i = " << i << endl;
+    // cout << "  *p = " << *p << endl;
+
+    passByRefToConstPtrToConst(p);
+
+    cout << "----passByRefToConstPtrToConst----" << endl;
+    cout << "   i = " << i << endl;
+    cout << "  *p = " << *p << endl;
+}
+
+int main()
+{
+    demoPass();
+
+    demoPassFancy();
 
     return 0;
 }
--- cpp/pass_by_ref_ptr/main.h
+++ cpp/pass_by_ref_ptr/main.h
@@ -4,10 +4,18 @@ int main();
 
 void passByVal(int i);
 
-void passByRef(int& i);
-void passByConstRef(const int& i);
+void passByRef(int &i);
+void passByConstRef(const int &i);
 
-void passByPtr(int* p);
-void passByPtrToConst(const int* p);
-void passByConstPtr(int* const p);
-void passByConstPtrToConst(const int* const p);
+void passByPtr(int *p);
+void passByPtrToConst(const int *p);
+void passByConstPtr(int *const p);
+void passByConstPtrToConst(const int *const p);
+
+void passByPtrToPtr(int** p);
+void passByRefToPtr(int*& p);
+void passByRefToPtrToConst(const int*& p);
+void passByRefToConstPtrToConst(const int* const& p);
+
+void demoPass();
+void demoPassFancy();
